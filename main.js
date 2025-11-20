6c8252dadca79eb156428761e0a7ccd514acc3fc
Yazid nursyafiqi <yazidnursyafiqi007@gmail.com>
2025-11-20 23:41:16 +0800

    commit pertama dan fitur basic

---
+++ main.js
@@ -0,0 +1,16 @@
+let count = 0;
+const display = document.getElementById('outputText');
+
+function update(){
+    display.innerHTML = count;
+}
+
+function tambah(){
+    count++;
+    update();
+}
+
+function kurang(){
+    count++;
+    update();
+}
