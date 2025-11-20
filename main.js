65766799dc81a2810c730aeb968f6f63ee982ed7
Yazid nursyafiqi <yazidnursyafiqi007@gmail.com>
2025-11-21 00:51:36 +0800

    menambahkan fitur checkpoint

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let count = 0;
 const display = document.getElementById('outputText');
+const log = document.querySelector('ul');
 
 function update(){
     display.innerHTML = count;
@@ -18,4 +19,16 @@ function kurang(){
 function reset(){
     count = 0;
     update();
+}
+
+
+function newList(){
+    const now = new Date();
+    const newPoint = document.createElement('li');
+    newPoint.innerHTML = `${now.getSeconds()}:${now.getMinutes()} = ${count}`;
+    log.appendChild(newPoint);
+}
+
+function resetList(){
+    log.innerHTML = '';
 }
